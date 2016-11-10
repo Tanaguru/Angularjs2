960fa3e4f99918f9ad30b32db420accacf08c0b6
amezigh <aallioui@oceaneconsulting.com>
2016-11-10 15:50:37 +0100

    Button toggle : maj texte du bouton

--- bouton-toggle/src/toggle.component.ts
+++ bouton-toggle/src/toggle.component.ts
@@ -8,14 +8,17 @@ import {Component, ElementRef} from 'angular2/core';
 export class ToggleComponent {
   active: boolean = false;
   targetId: string;
+  buttonText: string;
 
   constructor(elm: ElementRef) {
+    this.buttonText = 'Afficher le bloc';
     this.targetId = elm.nativeElement.getAttribute('target'); 
     let target = document.getElementById(this.targetId);
     target.setAttribute('class', target.getAttribute('class') + ' expanded-false');
   }
 
   toggleBlock() {
+    this.buttonText = this.active ? 'Afficher le bloc' : 'Masquer le bloc';
     let target = document.getElementById(this.targetId);
     target.setAttribute('class', 'block expanded-' + (this.active ? 'false' : 'true') );
     this.active = !this.active;
